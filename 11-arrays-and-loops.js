e74892b1f26e91491a985f0e82e485a30185204e
Galtbatzana <164704929+Galtbatzana@users.noreply.github.com>
2024-05-22 13:36:25 +0800

    todo list

---
+++ 11-arrays-and-loops.js
@@ -0,0 +1,34 @@
+
+// let i = 1;
+
+// while (i <=5) {
+//     console.log(i);
+//     i = i + 1;
+
+// }
+
+// let randomNumber = 0;
+
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i);
+// }
+
+// const todoList = [
+//     'make dinner',
+//     'wash dishes',
+//     'watch youtube'
+// ];
+
+// for (let i = 0; i <= 3; i++) {
+//     const value = todoList[i];
+//     console.log(value);
+// }
+
+const nums = [1, 1, 3];
+let total = 0;
+
+for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    total += num;
+} 
+    console.lod(total);
